b39da96204987f7e5238652810811cf8ae6678a7
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2026-05-20 17:41:39 +0200

    tools: fix sync script

--- tools/sync_workarea.sh
+++ tools/sync_workarea.sh
@@ -3,7 +3,6 @@
 rm -rf llm_fun/workarea/llm_fun/.git
 rsync -va --delete source/ llm_fun/workarea/llm_fun/source/
 rsync -a  dub.sdl llm_fun/workarea/llm_fun/dub.sdl
-rsync -a --exclude "build" --exclude "build_llama" llama_backend/ llm_fun/workarea/llm_fun/llama_backend/
 rsync -a linenoise.mak llm_fun/workarea/llm_fun/linenoise.mak
 rsync -a --delete --exclude ".git" vendor/ llm_fun/workarea/llm_fun/vendor/
 
