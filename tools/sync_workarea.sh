b7347bbd09f8755c0286cad756c85472fb7c473a
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2026-05-28 20:32:02 +0200

    devtools: fix sync script

--- tools/sync_workarea.sh
+++ tools/sync_workarea.sh
@@ -3,7 +3,8 @@
 rm -rf llmfun/workarea/llmfun/.git
 rsync -va --delete source/ llmfun/workarea/llmfun/source/
 rsync -a  dub.sdl llmfun/workarea/llmfun/dub.sdl
-rsync -a linenoise.mak llmfun/workarea/llmfun/linenoise.mak
+rsync -a linenoise.mak llmfun/workarea/llmfun/
+rsync -a sqlite3.mak llmfun/workarea/llmfun/
 rsync -a --delete --exclude ".git" vendor/ llmfun/workarea/llmfun/vendor/
 
 pushd llmfun/workarea/llmfun
